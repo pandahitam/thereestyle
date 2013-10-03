d86d84ffa8c540a3844a0e469fa353d0c28f37a1
chandra <chandra@licht-soft.com>
2013-10-03 14:33:15 +0700

    modul dan mail di theme rianascloset

---
+++ themes/rianascloset/mails/en/lang.php
@@ -0,0 +1,26 @@
+<?php
+
+global $_LANGMAIL;
+$_LANGMAIL = array();
+$_LANGMAIL['Welcome!'] = 'Welcome!';
+$_LANGMAIL['Process the payment of your order'] = 'Process the payment of your order';
+$_LANGMAIL['Message from contact form'] = '';
+$_LANGMAIL['Your message has been correctly sent #ct%1$s #tc%2$s'] = '';
+$_LANGMAIL['Your message has been correctly sent'] = '';
+$_LANGMAIL['New credit slip regarding your order'] = 'New credit slip regarding your order';
+$_LANGMAIL['Virtual product to download'] = 'Virtual product(s) available for download';
+$_LANGMAIL['Fwd: Customer message'] = 'Fwd: Customer message';
+$_LANGMAIL['Your guest account has been transformed to customer account'] = 'Your guest account has been transformed to a customer account';
+$_LANGMAIL['Package in transit'] = 'Package in transit';
+$_LANGMAIL['Log: You have a new alert from your shop'] = 'Log: You have a new alert from your shop';
+$_LANGMAIL['Your order has been changed'] = 'Your order has been changed';
+$_LANGMAIL['Order confirmation'] = 'Order confirmation';
+$_LANGMAIL['Message from a customer'] = 'Message from a customer';
+$_LANGMAIL['New message regarding your order'] = 'New message regarding your order';
+$_LANGMAIL['Your order return state has changed'] = 'Your order return status has changed';
+$_LANGMAIL['Your new password'] = 'Your new password';
+$_LANGMAIL['Password query confirmation'] = 'Forgot password';
+$_LANGMAIL['An answer to your message is available #ct%1$s #tc%2$s'] = 'An answer to your message is available #ct%1$s #tc%2$s';
+$_LANGMAIL['New voucher regarding your order %s'] = 'New voucher for your order %s';
+
+?>
